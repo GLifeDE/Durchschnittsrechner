dd33c7855a4cea969eed7d5464c6fc2670a33e82
BuildTools <unconfigured@null.spigotmc.org>
2021-04-24 13:12:06 +0200

    Projektdateien hinzufügen.

---
+++ Durchschnittsrechner/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Durchschnittsrechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Durchschnittsrechner V1.2";
+            
+            // Code
+            int count;
+
+            int countH;
+
+            double result;
+
+            List<string> Noten = new List<string>();
+
+            List<string> Hauptfachnoten = new List<string>();
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Notendurchschnittsrechner by Rafael Wickberg // GLifeDE");
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write("Wie viele Hauptfächer gibt es?: ");
+            countH = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Wie viele Nebenfächer gibt es?: ");
+            count = Convert.ToInt32(Console.ReadLine());
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            
+            for (int i = 0; i < countH; i++)
+            {
+                Console.Write("Gebe eine Hauptfachnote ein: ");
+                Hauptfachnoten.Add(Console.ReadLine());
+            }
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write("Gebe eine Nebenfachnote ein: ");
+                Noten.Add(Console.ReadLine());
+            }
+
+            double total = Noten.Sum(x => Convert.ToDouble(x));
+
+            double totalH = Hauptfachnoten.Sum(x => Convert.ToDouble(x));
+
+            
+
+            result = (((totalH * 2) + total) / (count+(countH*2)));
+
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("Der Durchschnitt beträgt " + Math.Round(result, 2));
+
+            if (result >= 3)
+            {
+                Console.WriteLine("Nächstes Mal wird's besser!");
+            }
+            else
+            {
+                Console.WriteLine("Sehr gut, weiter so!");
+            }
+         
+
+            //Warte bevor schließen
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Durchschnittsrechner/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("Durchschnittsrechner")]
+[assembly: AssemblyDescription("Dieses einfache Programm ermöglicht es seinen Notendurchschnitt mit ein paar wenigen und einfach Klicks auszurechnen!")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Durchschnittsrechner")]
+[assembly: AssemblyCopyright("Copyright GLifeDE ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("7965023b-e340-46a0-a227-826b722a8a0c")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.2")]
+[assembly: AssemblyFileVersion("1.2")]
+[assembly: NeutralResourcesLanguage("de-DE")]
