a55f6abc03fa4f768b41d1d2af180846bd83f9d0
Rafael <rafael36548@gmail.com>
2021-05-24 17:30:30 +0200

    changed Assembly

--- Durchschnittsrechner/Program.cs
+++ Durchschnittsrechner/Program.cs
@@ -8,7 +8,7 @@ namespace Durchschnittsrechner
     {
         static void Main(string[] args)
         {
-            Console.Title = "Durchschnittsrechner V4.2.1";
+            Console.Title = "Durchschnittsrechner V4.2.2";
 
             Program.Programm();
 
